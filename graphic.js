c9ae650117667bf2a1ebcdd574ad63d4c6c4f27d
Adam Satria <satriaadam04@gmail.com>
2022-09-06 14:50:28 +0700

    add vertex and fragment shadder

--- graphic.js
+++ graphic.js
@@ -1,7 +1,12 @@
 function main(){
     var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 200, 200);
+    var gl = canvas.getContext("webgl");
+    
+    //vertex shadder
+    var vertexShadderCode = "void main () {" + "}";
+    var fragmentShadderCode = `
+        void main(){   
+        }
+    `;
 }
 window.main();
