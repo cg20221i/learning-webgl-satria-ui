d56a5511525bfde24d1f61a078c3dccf6a0af41e
Adam Satria <satriaadam04@gmail.com>
2022-09-06 14:27:18 +0700

    adding js file

---
+++ graphic.js
@@ -0,0 +1,7 @@
+function main(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 200, 200);
+}
+window.main();
